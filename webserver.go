fc27bcf84f3ed0494255508464dae01f86044402
Oleg Sher <github@sher.biz>
2019-11-30 22:14:55 +0200

    Webserver/branch test

---
+++ webserver.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("test")
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprint(w, "Welcome to my website!")
+	//})
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	//fs := http.FileServer(http.Dir("static/"))
+	//http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8081", nil)
+	time.Sleep(10 * time.Second)
+}
